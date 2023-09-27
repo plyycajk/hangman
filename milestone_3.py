4c2faccad399ba6555c9b8ff8409699e7c3f1c28
plyycajk <colinknight1985@gmail.com>
2023-09-27 21:11:19 +0100

    updates guess variable name to letter_guess

--- milestone_3.py
+++ milestone_3.py
@@ -5,19 +5,19 @@ word_list = ["satsuma", "plum", "nectarine", "apple", "grapes"]
 word = random.choice(word_list)
 
 
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.")
+def check_guess(letter_guess):
+    letter_guess = letter_guess.lower()
+    if letter_guess in word:
+        print(f"Good guess! {letter_guess} is in the word.")
     else:
-        print(f"Sorry, {guess} is not in the word, try again")
+        print(f"Sorry, {letter_guess} is not in the word, try again")
     
 
 def ask_for_input():
     while True:
-         guess = input("Please, guess a letter")
-         if guess.isalpha() == True and len(guess) == 1:
-             check_guess(guess)
+         letter_guess = input("Please, guess a letter")
+         if letter_guess.isalpha() == True and len(letter_guess) == 1:
+             check_guess(letter_guess)
              return     
          else:
             print("Invalid letter. Please, enter a single alphabetical character.")
